af292f1dfad3b6a47aa9032fce977d8820596402
Dmitriy Talankin <dtalankin@placeiq.com>
2016-02-20 00:54:15 +0600

    practice 7 in progress. Tests with parameters in progress

--- src/test/java/ru/dtalankin/trainings/FileTest.java
+++ src/test/java/ru/dtalankin/trainings/FileTest.java
@@ -4,11 +4,15 @@
 
 package ru.dtalankin.trainings;
 
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import ru.dtalankin.trainings.categories.BrokenTests;
 import ru.dtalankin.trainings.categories.NegativeTests;
@@ -18,15 +22,24 @@ import java.io.File;
 import java.io.IOException;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(DataProviderRunner.class)
 public class FileTest extends FileFixture {
+    @DataProvider
+    public static Object[][] paths() {
+        return new Object[][] {
+                {"dir1", "abracadabra.txt"},
+                {"dir2", "tumtumtum.txt"}
+        };
+    }
+
     @Test
     @Category(PositiveTests.class)
-    public void filePositiveTest1() throws IOException {
+    @UseDataProvider("paths")
+    public void filePositiveTest1(String subPath, String fileName) throws IOException {
         System.out.println("PositiveTest1");
-        String subPath = "subPath";
         File fullPath = new File(tmpDir.toFile(), subPath);
         fullPath.mkdir();
-        File file = new File(fullPath, "file1.txt");
+        File file = new File(fullPath, fileName);
         file.createNewFile();
         Assert.assertTrue("File1 is not created", file.exists());
     }
@@ -43,7 +56,7 @@ public class FileTest extends FileFixture {
     @Test
     @Category({BrokenTests.class})
     public void filePositiveTest3() throws IOException {
-        System.out.println("BrokenTest3");
+        System.out.println("PositiveTest3 - BrokenTest");
         File file = new File(subDir, "file3.txt");
         file.createNewFile();
         Assert.assertTrue("File4 does not exist", file.exists());
