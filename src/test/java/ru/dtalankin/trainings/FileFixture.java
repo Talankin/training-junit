c71d7313beca290fd5514062ec4c2fba0d6620dc
dtalankin <dtalankin@placeiq.com>
2016-02-22 11:27:05 +0600

    practice 8 done

--- src/test/java/ru/dtalankin/trainings/FileFixture.java
+++ src/test/java/ru/dtalankin/trainings/FileFixture.java
@@ -7,6 +7,8 @@ package ru.dtalankin.trainings;
 import org.apache.commons.io.FileUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.rules.ExternalResource;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,18 +20,21 @@ public class FileFixture {
     protected static File subDir = null;
     protected static String notExistingDir = "qwerty";
 
-    @BeforeClass
-    public static void createTmpDirTest() throws IOException {
-        System.out.println("SetUp Fixture");
-        tmpDir = Files.createTempDirectory("tmpDir");
-        subDir = new File(tmpDir.toFile(), "subdir");
-        subDir.mkdir();
-    }
+    @ClassRule
+    public static ExternalResource fileRule = new ExternalResource() {
+        @Override
+        protected void before() throws Throwable {
+            System.out.println("SetUp Fixture");
+            tmpDir = Files.createTempDirectory("tmpDir");
+            subDir = new File(tmpDir.toFile(), "subdir");
+            subDir.mkdir();
+        }
 
-    @AfterClass
-    public static void deleteTmpDirTest(){
-        System.out.println("TearDown Fixture");
-        FileUtils.deleteQuietly(tmpDir.toFile());
-    }
+        @Override
+        protected void after() {
+            System.out.println("TearDown Fixture");
+            FileUtils.deleteQuietly(tmpDir.toFile());
+        }
+    };
 
 }
